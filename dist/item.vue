be31df389984615eaa19e8d97da45dfa918a4070
Jerret321 <321jiangtao@gmail.com>
2017-01-04 19:48:05 +0800

    1.0.0

--- dist/item.vue
+++ dist/item.vue
@@ -6,19 +6,14 @@
 </template>
 <script>
     export default{
-        render(h) {
-            return h('li', {
-                class: 'vt-menu__item'
-            }, this.$slots.default)
-        },
         data(){
             return {
-                classObj: ['vt-menu__item']
+                classObj: ['ui-menu__item']
             }
         },
         mounted() {
             if(location.href.indexOf(this.$el.querySelector('a').href) > -1){
-                this.classObj.push('vt-menu__item--active')
+                this.classObj.push('ui-menu__item--active')
                 this.$parent.openParent()
             }
         }
