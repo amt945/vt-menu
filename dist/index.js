fbc5d43615a2dece7a956077b56fd8599ff7309f
Jerret321 <321jiangtao@gmail.com>
2017-01-05 16:31:13 +0800

    output multiple component

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.Submenu = exports.Item = exports.Menus = undefined;
+
+var _index = require('./index.vue');
+
+var _index2 = _interopRequireDefault(_index);
+
+var _item = require('./item.vue');
+
+var _item2 = _interopRequireDefault(_item);
+
+var _submenu = require('./submenu.vue');
+
+var _submenu2 = _interopRequireDefault(_submenu);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Menus = _index2.default;
+exports.Item = _item2.default;
+exports.Submenu = _submenu2.default;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Menus from './index.vue'
+import Item from './item.vue'
+import Submenu from './submenu.vue'
+
+export {
+	Menus,
+	Item,
+	Submenu
+}
